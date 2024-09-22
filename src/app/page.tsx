0bd24283e4025d94448a93d5c145e5d89a684bcc
Carlos LL <carlosll2493@gmail.com>
2024-09-22 17:55:17 -0500

    Refactor mockdata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,46 @@ import HeroWithCTA from "@/components/HeroWithCTA";
 import img1 from "@/assets/images/home/img1.jpg";
 import img2 from "@/assets/images/home/img2.jpg";
 import img3 from "@/assets/images/home/img3.jpg";
+import { StaticImageData } from "next/image";
 
 const HeroWithCTATitle = "Delight in Every Bite!"
 const HeroWithCTABody = "Discover the perfect blend of fresh ingredients and authentic flavors. Whether you're here for a quick bite or a hearty meal, every dish is crafted to satisfy your cravings and leave you wanting more."
 
-const dishBody1 = "Indulge in our expertly grilled dishes, where every bite brings the rich, smoky essence of the barbecue straight to your plate. Perfect for meat lovers!"
-const dishBody2 = "Savor our succulent chicken dishes, marinated to perfection and cooked just right. From classic favorites to unique creations, there's something for everyone!"
-const dishBody3 = "Treat yourself to our creamy, delicious shakes made with premium ingredients. Whether you prefer fruity or indulgent, our shakes are the perfect way to cool down and satisfy your cravings."
+const dishcards: { title: string, image?: StaticImageData, subtitle?: string, body: string, topBorderColor?: string, link?: string }[] = [
+  {
+    title: "Grill",
+    image: img1,
+    subtitle: "Smoky Flavors Await",
+    body: "Indulge in our expertly grilled dishes, where every bite brings the rich, smoky essence of the barbecue straight to your plate. Perfect for meat lovers!",
+    topBorderColor: "#f5261f",
+    link: "/dishes",
+  },
+  {
+    title: "Chicken",
+    image: img2,
+    subtitle: "Smoky Flavors Await",
+    body: "Savor our succulent chicken dishes, marinated to perfection and cooked just right. From classic favorites to unique creations, there's something for everyone!",
+    topBorderColor: "#facc5c",
+    link: "/dishes",
+  },
+  {
+    title: "Shakes",
+    image: img3,
+    subtitle: "Sweet Refreshment",
+    body: "Treat yourself to our creamy, delicious shakes made with premium ingredients. Whether you prefer fruity or indulgent, our shakes are the perfect way to cool down and satisfy your cravings.",
+    topBorderColor: "#abda1e",
+    link: "/dishes",
+  },
+]
 
 export default function Page() {
   return (
     <div className="flex flex-col justify-center items-center">
       <HeroWithCTA title={HeroWithCTATitle} body={HeroWithCTABody} />
       <Container className="flex gap-9 w-full justify-center items-center">
-        <DishCard title={"Grill"} body={dishBody1} image={img1} subtitle="Smoky Flavors Await" link="/dishes" topBorderColor="#f5261f"/>
-        <DishCard title={"Chickem"} body={dishBody2} image={img2} subtitle="Cluckin’ Good Choices" link="/dishes" topBorderColor="#facc5c"/>
-        <DishCard title={"Shakes"} body={dishBody3} image={img3} subtitle="Sweet Refreshment" link="/dishes" topBorderColor="#abda1e"/>
+        {dishcards.map((card, index) => (
+          <DishCard key={index} {...card} />
+        ))}
       </Container>
     </div>
   )
--- src/components/DishCard/index.tsx
+++ src/components/DishCard/index.tsx
@@ -1,7 +1,7 @@
 import Image, { StaticImageData } from "next/image"
 import Link from "next/link"
 
-interface Props{
+interface Props {
   title: string
   image?: StaticImageData
   subtitle?: string
@@ -10,14 +10,21 @@ interface Props{
   link?: string
 }
 
-function DishCard ({ title, image, subtitle, body, topBorderColor, link }: Props) {
+function DishCard({ title, image, subtitle, body, topBorderColor, link }: Props) {
   return (
-    <div className="border-t-8 flex flex-col gap-4 py-6" style={{ borderColor: topBorderColor}}>
+    <div className="border-t-8 flex flex-col gap-4 py-6" style={{ borderColor: topBorderColor }}>
       <h2 className="text-4xl text-gray-700">{title}</h2>
-      {image && <Image src={image?.src ?? ""} alt={title} width={image?.width} height={image?.height}/>}
-      {subtitle &&<h3>{subtitle}</h3>}
+      {image && <img src={image?.src ?? ""} alt={title} width={image?.width} height={image?.height} style={{ maxHeight: "215px" }} />}
+      {subtitle && <h3>{subtitle}</h3>}
       <p className="text-gray-500">{body}</p>
-      {link && <Link href={link}><button className="text-white py-2 px-4 rounded-sm" style={{ backgroundColor: topBorderColor}}>View More</button></Link>}
+      {
+        link &&
+        <Link href={link}>
+          <button className="text-white py-2 px-4 rounded-sm" style={{ backgroundColor: topBorderColor }}>
+            View More
+          </button>
+        </Link>
+      }
     </div>
   )
 }
