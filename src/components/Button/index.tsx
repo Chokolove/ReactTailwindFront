75a8850eb2e424e69dd3e98357b2c196a788d664
Carlos LL <carlosll2493@gmail.com>
2024-09-22 19:02:27 -0500

    Add reactHook and refactor email subscription

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,12 +2,16 @@ interface Props {
   text: string;
   className?: string;
   style?: React.CSSProperties;
+  onClick?: () => void;
+  type?: "submit" | "button" | "reset" | undefined;
 }
-function Button({ text, className, style }: Props) {
+function Button({ text, className, style, onClick, type="button" }: Props) {
   return (
     <button
       className={`bg-gray-700 text-white py-2 px-4 rounded-sm hover:opacity-80 transition duration-300 ease-in-out ${className}`}
-      style={style}>
+      style={style}
+      type={type}
+      onClick={onClick}>
       {text}
     </button>
   )
--- src/components/EmailSubscription/index.tsx
+++ src/components/EmailSubscription/index.tsx
@@ -1,14 +1,35 @@
+'use client';
+
+import { useForm, SubmitHandler } from "react-hook-form";
 import Button from "../Button"
 import Container from "../Container"
 
-function EmailSubscription () {
+type Inputs = {
+  email: string
+}
+
+function EmailSubscription() {
+  const {
+    register,
+    handleSubmit,
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data, e) => {
+    e?.preventDefault()
+    console.log(data)
+  }
   return (
     <div className="w-full bg-lightGray">
-      <Container className="flex gap-9 justify-center items-center">
-        <h2 className="text-2xl text-gray-700">Stay Updated with Our Latest Offers</h2>
-        <input type="text" placeholder="Enter your email" className="border border-gray-200 py-2 px-4 rounded-sm w-full max-w-80" />
-        <Button text="Subscribe Now" className="w-80" />
-      </Container>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Container className="flex gap-9 justify-center items-center">
+          <h2 className="text-2xl text-gray-700">Stay Updated with Our Latest Offers</h2>
+          <input 
+            type="email" 
+            placeholder="Enter your email" 
+            className="border border-gray-200 py-2 px-4 rounded-sm w-full max-w-80"
+            {...register("email")}/>
+          <Button text="Subscribe Now" className="w-80" type="submit"/>
+        </Container>
+      </form>
     </div>
   )
 }
